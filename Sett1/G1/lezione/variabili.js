b3c3f6c90278950fb8739a6f6bcf8bd74a23cb99
Naviganet <dadegi@gmail.com>
2023-11-20 11:22:06 +0100

    Codice UNIT 3 settimana 1 giorno 1 - lezione

---
+++ Sett1/G1/lezione/variabili.js
@@ -0,0 +1,15 @@
+var saluto = 'Hello World!';
+// TIPI IN TYPESCRIPT (primitivi)
+var stringa;
+var numero;
+var booleano;
+var indefinito;
+var nullo;
+var nonLoSo; // Il tipo any può cambiare nel corso del flusso
+var nonLoSapro;
+// TIPI DERIVATI O COMPLESSI
+var array;
+var oggetto;
+console.log(saluto);
+saluto = 5;
+console.log(saluto);
---
+++ Sett1/G1/lezione/variabili.ts
@@ -0,0 +1,21 @@
+let saluto: any = 'Hello World!';
+
+// TIPI IN TYPESCRIPT (primitivi)
+let stringa: string;
+let numero: number;
+let booleano: boolean;
+let indefinito: undefined;
+let nullo: null;
+let nonLoSo: any; // Il tipo any può cambiare nel corso del flusso
+let nonLoSapro: never;
+
+// TIPI DERIVATI O COMPLESSI
+let array: [];
+let oggetto: {};
+
+console.log(saluto);
+
+saluto = 5;
+
+console.log(saluto);
+

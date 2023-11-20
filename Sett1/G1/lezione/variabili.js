b71f5829fa0575b2c17cdbce12d17872b1f76619
Naviganet <dadegi@gmail.com>
2023-11-20 12:01:34 +0100

    COdice UNIT 3 settimana 1 giorno 1 - lezione

--- Sett1/G1/lezione/variabili.js
+++ Sett1/G1/lezione/variabili.js
@@ -7,9 +7,27 @@ var indefinito;
 var nullo;
 var nonLoSo; // Il tipo any può cambiare nel corso del flusso
 var nonLoSapro;
+var costante = 'Sono una stringa costante';
 // TIPI DERIVATI O COMPLESSI
 var array;
 var oggetto;
 console.log(saluto);
 saluto = 5;
 console.log(saluto);
+// Operatore ternario in typescript
+// Funziona con entrambe le sintassi; quella a riga 26 è la sintassi letterale Typescript
+var exp = 5 < 3
+    ? 'Il primo numero è minore del secondo'
+    : 'Il secondo numero è minore del primo';
+// let exp = (5<3) ? true: false;
+console.log(exp);
+var a = 8;
+var b = 5;
+var operazione = function (num1, num2) {
+    var risultato = num1 - num2;
+    console.log(risultato);
+    return risultato;
+};
+// Il ternario verifica se il risultato della funzione è un numero positivo: se sì, esegue la funzione, altrimenti produce una stringa
+var verifica = a > b ? operazione(a, b) : 'La funzione produce un numero negativo';
+console.log(verifica);
--- Sett1/G1/lezione/variabili.ts
+++ Sett1/G1/lezione/variabili.ts
@@ -8,6 +8,7 @@ let indefinito: undefined;
 let nullo: null;
 let nonLoSo: any; // Il tipo any può cambiare nel corso del flusso
 let nonLoSapro: never;
+const costante: string = 'Sono una stringa costante';
 
 // TIPI DERIVATI O COMPLESSI
 let array: [];
@@ -19,3 +20,28 @@ saluto = 5;
 
 console.log(saluto);
 
+// Operatore ternario in typescript
+// Funziona con entrambe le sintassi; quella a riga 26 è la sintassi letterale Typescript
+
+let exp: string =
+	5 < 3
+		? 'Il primo numero è minore del secondo'
+		: 'Il secondo numero è minore del primo';
+// let exp = (5<3) ? true: false;
+
+console.log(exp);
+
+let a: number = 8;
+let b: number = 5;
+
+const operazione = (num1: number, num2: number): number => {
+	let risultato = num1 - num2;
+	console.log(risultato);
+	return risultato;
+};
+
+// Il ternario verifica se il risultato della funzione è un numero positivo: se sì, esegue la funzione, altrimenti produce una stringa
+const verifica: any =
+	a > b ? operazione(a, b) : 'La funzione produce un numero negativo';
+
+console.log(verifica);

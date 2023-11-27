7bd4430530eb111faef1954db81cc8fcc0e0f703
Naviganet <dadegi@gmail.com>
2023-11-27 13:56:45 +0100

    Codice lezione UNIT 3 settimana 2 giorno 1

--- Sett2/G1/lezione/src/app/app.module.ts
+++ Sett2/G1/lezione/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component,
+    Componente2Component
   ],
   imports: [
     BrowserModule

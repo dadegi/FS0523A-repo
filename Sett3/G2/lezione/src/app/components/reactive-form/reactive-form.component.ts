922d3765f2d1731d9ff0410bb97db206f4e3c932
Naviganet <dadegi@gmail.com>
2023-12-05 12:55:17 +0100

    Codice lezione UNIT 3 settimana 3 giorno 2

--- Sett3/G2/lezione/src/app/components/reactive-form/reactive-form.component.ts
+++ Sett3/G2/lezione/src/app/components/reactive-form/reactive-form.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
-  selector: 'app-reactive-form',
-  templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.scss']
+    selector: 'app-reactive-form',
+    templateUrl: './reactive-form.component.html',
+    styleUrls: ['./reactive-form.component.scss'],
 })
 export class ReactiveFormComponent implements OnInit {
 
-  constructor() { }
+    form!: FormGroup;
+    generi = ['uomo', 'donna'];
+    userProibiti = ['Pippo', 'Pluto'];
 
-  ngOnInit(): void {
-  }
+    constructor(private fb: FormBuilder) {}
 
+    validUsername = (formC: FormControl) => {
+        if (this.userProibiti.includes(formC.value)) {
+            return {'usernameProibito': true};
+        } else {
+            return null;
+        }
+    }
+
+    ngOnInit(): void {
+        this.form = this.fb.group({
+            userInfo: this.fb.group({
+                username: this.fb.control(null, [Validators.required, this.validUsername]),
+                email: this.fb.control(null, [Validators.required, Validators.email])
+            }),
+            genere: this.fb.control('donna'),
+            sports: this.fb.array([])
+        });
+    }
+
+    getErrorsC(name: string, error: string) {
+        return this.form.get(name)?.errors![error];
+    }
+
+    getFormC(name: string) {
+        return this.form.get(name);
+    }
+
+    getSportsC() {
+        return (this.form.get('sports') as FormArray).controls;
+    }
+
+    addSports() {
+        const control = this.fb.control(null);
+        (this.form.get('sports') as FormArray).push(control);
+        console.log(this.getSportsC());
+    }
+
+    submit() {
+        console.log(this.form);
+        console.log('Form correttamente inviato');
+        this.form.reset();
+    }
 }
